d91383bdf1bdf6948097bd8f2217b851dbeedee4
brayan monje garrido <neymar.jhon@gmail.com>
2025-11-22 06:08:53 -0300

    Update TestRunner.java

--- src/test/java/com/platinum/runner/TestRunner.java
+++ src/test/java/com/platinum/runner/TestRunner.java
@@ -1,13 +1,20 @@
 package com.platinum.runner;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.*;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.platinum.steps",
-        plugin = {"pretty", "html:target/cucumber-report.html"}
+    features = "src/test/resources/features",
+    glue = {"com.platinum.steps"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports.html",
+        "json:target/cucumber-reports.json",
+        "junit:target/surefire-reports/cucumber.xml"
+    },
+    monochrome = true
 )
 public class TestRunner {
 }

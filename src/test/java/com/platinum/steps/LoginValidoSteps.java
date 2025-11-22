a8efcb10c7a5eb4e522cfdad59a4ef8c66bb1776
brayan monje garrido <neymar.jhon@gmail.com>
2025-11-22 06:33:39 -0300

    Update LoginValidoSteps.java

--- src/test/java/com/platinum/steps/LoginValidoSteps.java
+++ src/test/java/com/platinum/steps/LoginValidoSteps.java
@@ -4,34 +4,34 @@ import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginValidoSteps {
+public class LoginIncorrectoSteps {
 
     WebDriver driver;
 
-    @Given("que abro el navegador y accedo a la página de login")
-    public void abrirLogin() {
+    @Given("que abro el navegador y accedo a la página de login con credenciales incorrectas")
+    public void abrirLoginIncorrecto() {
         driver = new ChromeDriver();
         driver.get("http://localhost:8080/CtaCorriente/login.jsp");
     }
 
-    @When("ingreso el nombre de usuario {string}")
-    public void ingresoUsuario(String usuario) {
+    @When("ingreso un nombre de usuario incorrecto {string}")
+    public void ingresoUsuarioIncorrecto(String usuario) {
         driver.findElement(By.id("usuario")).sendKeys(usuario);
     }
 
-    @When("ingreso la contraseña {string}")
-    public void ingresoPassword(String pass) {
+    @When("ingreso una contraseña incorrecta {string}")
+    public void ingresoPasswordIncorrecto(String pass) {
         driver.findElement(By.id("password")).sendKeys(pass);
     }
 
-    @When("presiono el botón de ingresar")
-    public void presionarLogin() {
+    @When("presiono el botón de ingresar con credenciales incorrectas")
+    public void presionarLoginIncorrecto() {
         driver.findElement(By.id("btnLogin")).click();
     }
 
-    @Then("debo ver la página del menú principal")
-    public void verMenuPrincipal() {
-        driver.getPageSource().contains("Bienvenido");
+    @Then("debo ver un mensaje de error de credenciales")
+    public void verMensajeError() {
+        driver.getPageSource().contains("Error");
         driver.quit();
     }
 }
